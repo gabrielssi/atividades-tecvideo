bca959ebf860ef59db5f5091e0ff06f4fbac3cfe
Gabriel Souza <gabrielsouzadasilva30@gmail.com>
2023-07-26 22:45:13 -0300

    Atividade diagonalDifference do HackerRank

---
+++ diagonalDifference.js
@@ -0,0 +1,16 @@
+function diagonalDifference(arr) {
+  let leftDiagonal = 0;
+  let rightDiagonal = 0;
+
+  for (let index = 0; index < arr.length; index++) {
+    leftDiagonal += arr[index][index];
+    rightDiagonal += arr[index][arr.length - 1 - index]
+  }
+
+  return Math.abs(leftDiagonal - rightDiagonal);
+}
+
+console.log(diagonalDifference([
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]]));
